8544177fdee28e9568a1f139932f26338ff5d802
ivan <tsymbaliuk.nau@gmail.com>
2022-11-27 22:07:07 +0200

    Allowing Users to Enter Data, Topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """The home page for Learning Log"""
@@ -18,4 +19,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
-# Create your views here.
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+    # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+    # POST data submitted; process data.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_log:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
