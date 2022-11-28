8a332c96846ef821339095476aa89bdd26f2bfd0
ivan <tsymbaliuk.nau@gmail.com>
2022-11-28 13:00:55 +0200

    Protecting the edit_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
-
+from django.http import Http404
 
 def index(request):
     """The home page for Learning Log"""
@@ -12,7 +12,7 @@ def index(request):
 @login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -21,6 +21,9 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -36,8 +39,12 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
+            # form.save()
             return redirect('learning_log:topics')
+
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
@@ -69,6 +76,9 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
